7ebdd7ef38692dbcee97d05999a25f4c4e7b9c4c
sohail019 <shaikhhsohail0193@gmail.com>
2024-09-30 19:04:53 +0530

    Learned about how to extend interface and what is type aliases

--- 02_Interfaces/interfaces.js
+++ 02_Interfaces/interfaces.js
@@ -10,3 +10,22 @@ function person(obj) {
 person({ name: "sohail", email: "sohail@gmail.com", age: 23, gender: "male" });
 function user(obj) { }
 user({ name: "sohail", email: "sohail@gmail.com", age: 23 });
+function profile(obj) {
+    console.log(obj.name);
+    console.log(obj.email);
+    console.log(obj.age);
+    console.log(obj.gender);
+    console.log(obj.admin);
+}
+function abcde(obj) {
+    obj.name;
+    obj.email;
+}
+let num1;
+//? but this above example is senseless to understand.. let's see how we can use type aliases  to do it..
+let anything;
+let anythingg;
+//? we can use this in function argument too
+function anythinggg(obj) { }
+anythinggg("sohail");
+anythinggg(98);
--- 02_Interfaces/interfaces.ts
+++ 02_Interfaces/interfaces.ts
@@ -32,3 +32,52 @@ interface User {
 function user(obj: User){}
 
 user({name: "sohail", email: "sohail@gmail.com", age: 23})
+
+//* Extending Interfaces - we can use extends keyword to extend interfaces
+
+interface Admin extends User{
+    admin: boolean
+}
+
+function profile(obj: Admin){
+    console.log(obj.name)
+    console.log(obj.email)
+    console.log(obj.age)
+    console.log(obj.gender)
+    console.log(obj.admin)
+}
+
+//* Merged Interfaces example
+
+interface Abcde {
+    name: string
+}
+
+interface Abcde {
+    email: string
+}
+
+function abcde(obj: Abcde){
+    obj.name
+    obj.email
+}
+//! Above example has issue as we cannot keep same name in different interfaces..
+
+//* Type Aliases - used to create aliases for a type.. 
+
+type sankhya = number
+let num1: sankhya
+
+//? but this above example is senseless to understand.. let's see how we can use type aliases  to do it..
+
+let anything: string | number | null
+
+//* instead of that we can use type aliases
+type value = string | number | null
+let anythingg: value
+
+//? we can use this in function argument too
+function anythinggg(obj: value){}
+anythinggg("sohail")
+anythinggg(98)
+
