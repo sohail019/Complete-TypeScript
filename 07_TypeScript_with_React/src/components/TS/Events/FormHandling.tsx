f15fc35ad9883ac2f0df5c826cc25870cc53da8c
sohail019 <shaikhhsohail0193@gmail.com>
2024-10-06 13:15:58 +0530

    Learned about Form Handling

--- 07_TypeScript_with_React/src/components/TS/Events/FormHandling.tsx
+++ 07_TypeScript_with_React/src/components/TS/Events/FormHandling.tsx
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
+
 export const FormHandling: React.FC = () => {
+
+  const [name, setName] = useState<string>("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) : void => {
+    setName(e.target.value)
+  }
+
   return (
   <div>
     <h1>Form Handling</h1>
+    <input type="text" placeholder="Enter Name" onChange={handleChange} />
+    <p>Name: {name}</p>
   </div>
   );
 };
